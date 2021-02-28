42874f21ec5474758c77f48d8290146c4e57e3ee
Gaurav singh <gks.com131@gmail.com>
2021-02-28 22:38:27 +0530

    adding mail and order

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,5 @@
 import io.javalin.Javalin;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -8,13 +9,50 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
-
+        SendEmail mailClient = new SendEmail();
+        app.post("/order/", ctx -> {
+            JSONObject file = new JSONObject();
+            try{
+                JSONParser parser = new JSONParser();
+                String details = ctx.body();
+                JSONObject jsonObject = (JSONObject) parser.parse(ctx.body());
+                Connection c = DriverManager
+                        .getConnection("jdbc:postgresql://localhost:5432/ecom",
+                                "postgres", "qwerty");
+                Statement stmt = c.createStatement();
+                stmt.executeUpdate("create table if not exists order(id SERIAL primary key not null, " +
+                                        "details TEXT not null)");
+                stmt.close();
+                PreparedStatement st = c.prepareStatement("INSERT INTO order(details) values(?)");
+                st.setString(1, details);
+                st.executeUpdate();
+                st.close();
+                JSONArray products_details = (JSONArray) jsonObject.get("product_details");
+                for (int i = 0; i < products_details.size(); i++){
+                    st = c.prepareStatement("UPDATE product SET quantity=? WHERE id=?");
+                    JSONObject obj = (JSONObject) products_details.get(i);
+                    st.setLong(1, (Long) obj.get("new_quantity"));
+                    st.executeUpdate();
+                    st.close();
+                    int mailStatus = (int) obj.get("mail_status");
+                    if (mailStatus == 1){
+                        mailClient.Send(String.format("Quantity for item %s product id %s is really low",
+                                        obj.get("id"), obj.get("name")));
+                    }
+                }
+                c.close();
+                file.put("success", 1);
+            }
+            catch (Exception e){
+                System.out.println(e);
+                file.put("success", 0);
+            }
+            ctx.json(file);
+        });
 
         app.get("/categories/", ctx -> {
             JSONObject file = new JSONObject();
-            Connection c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/ecom",
-                            "postgres", "qwerty");
+            Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ecom", "postgres", "qwerty");
             Statement  stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery( "select DISTINCT on (category) category, image from product;" );
             while ( rs.next() ) {
@@ -25,6 +63,56 @@ public class HelloWorld {
             stmt.close();
             c.close();
             ctx.json(file);
+        });
+
+        app.put("/update/", ctx -> {
+            JSONObject file = new JSONObject();
+            try{
+                JSONParser parser = new JSONParser();
+                JSONObject jsonObject = (JSONObject) parser.parse(ctx.body());
+
+                Connection c = DriverManager
+                        .getConnection("jdbc:postgresql://localhost:5432/ecom",
+                                "postgres", "qwerty");
+                PreparedStatement st = c.prepareStatement("UPDATE product SET name=?, description=?, category=?," +
+                        "price=?, quantity=?, min_quantity=? WHERE id=?");
+                st.setString(1, (String) jsonObject.get("name"));
+                st.setString(2, (String) jsonObject.get("description"));
+                st.setString(3, (String) jsonObject.get("category"));
+                st.setLong(4, (Long) jsonObject.get("price"));
+                st.setLong(5, (Long) jsonObject.get("quantity"));
+                st.setLong(6, (Long) jsonObject.get("min_quantity"));
+                st.setLong(7, (Long) jsonObject.get("id"));
+                st.executeUpdate();
+                st.close();
+                c.close();
+                file.put("success", 1);
+            }
+            catch (Exception e){
+                System.out.println(e);
+                file.put("success", 0);
+            }
+            ctx.json(file);
+        });
+
+        app.delete("/delete/:id", ctx -> {
+            JSONObject file = new JSONObject();
+            int productId = Integer.parseInt(ctx.pathParam("id"));
+            try{
+                Connection c = DriverManager
+                        .getConnection("jdbc:postgresql://localhost:5432/ecom",
+                                "postgres", "qwerty");
+                PreparedStatement st = c.prepareStatement("DELETE from product where id=?");
+                st.setInt(1, productId);
+                st.executeUpdate();
+                st.close();
+                c.close();
+                file.put("success", 1);
+            }
+            catch (Exception e){
+               System.out.println(e);
+               file.put("success", 0);
+            }
         });
 
         app.post("/login/", ctx -> {
@@ -49,6 +137,7 @@ public class HelloWorld {
                 else{
                     file.put("success", 0);
                 }
+                st.close();
                 c.close();
 
             }
@@ -107,24 +196,33 @@ public class HelloWorld {
                         "min_quantity INT not null default 0)");
                 stmt.close();
                 PreparedStatement st = c.prepareStatement("INSERT INTO product(name, description, category, " +
-                        "price, image, quantity, min_quantity) values(?, ?, ?, ?, ?, ?, ?)");
+                                                "price, image, quantity, min_quantity) values(?, ?, ?, ?, ?, ?, ?)");
                 st.setString(1, (String) jsonObject.get("name"));
                 st.setString(2, (String) jsonObject.get("description"));
                 st.setString(3, (String) jsonObject.get("category"));
                 st.setLong(4, (Long) jsonObject.get("price"));
                 st.setString(5, (String) jsonObject.get("image"));
-                st.setString(6, (String) jsonObject.get("quantity"));
-                st.setString(7, (String) jsonObject.get("min_quantity"));
+                st.setLong(6, (Long) jsonObject.get("quantity"));
+                st.setLong(7, (Long) jsonObject.get("min_quantity"));
                 st.executeUpdate();
                 st.close();
                 c.close();
                 file.put("success", 1);
             }
             catch (Exception e){
+                System.out.println(e);
                 file.put("success", 0);
             }
             ctx.json(file);
         });
     }
+}
+interface Order{
+
+}
+
+interface Product{
+    String name = null;
+
 
 }
---
+++ src/main/java/SendEmail.java
@@ -0,0 +1,33 @@
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+
+public class SendEmail {
+    public boolean Send(String message){
+        try {
+            Email email = EmailBuilder.startingBlank()
+                    .to("res", "researchvkj@gmail.com")
+                    .withSubject("hey")
+                    .withPlainText(message)
+                    .withHeader("X-Priority", 5)
+                    .from("researchvkj@gmail.com")
+                    .buildEmail();
+
+            Mailer mailer = MailerBuilder
+                    .withSMTPServer("smtp.gmail.com", 587, "researchvkj@gmail.com", "Niltech@12345")
+                    .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                    .withSessionTimeout(10 * 1000)
+                    .buildMailer();
+
+            mailer.sendMail(email);
+            return true;
+        }
+        catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+
+    }
+}
